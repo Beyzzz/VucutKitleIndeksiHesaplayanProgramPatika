3e47bf19639fa955115f634bb4677aef4becc681
Beyzanur <75632809+Beyzzz@users.noreply.github.com>
2022-10-31 16:35:10 +0300

    Patika

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double boy, kilo, indeks;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Lütfen boyunuzu metre cinsinden giriniz: ");
+		boy = input.nextDouble();
+
+		System.out.print("Lütfen kilonuzu giriniz: ");
+		kilo = input.nextDouble();
+
+		indeks = (kilo / (boy * boy));
+		System.out.print("Vücut Kitle İndeksiniz:" + indeks);
+
+	}
+
+}
